52274997bd5e94b7da8ca8bb36309fd230dd33f2
HIEUTNSE61591\HieuTNSE61591 <hieutn212@gmail.com>
2018-04-24 14:07:46 +0700

    Fix bug direction

--- app/src/main/java/com/example/project/mobilecapstone/Data/sharedData.java
+++ app/src/main/java/com/example/project/mobilecapstone/Data/sharedData.java
@@ -7,7 +7,7 @@ import android.os.Environment;
  */
 
 public class sharedData {
-    public static String IP = "b7181e35.ngrok.io";
+    public static String IP = "324f5c7c.ngrok.io";
     public static String storage = Environment.getExternalStorageDirectory().getAbsolutePath() + "/LOAB/";
     //tracking location
     public static Double LAT = 10.0;
